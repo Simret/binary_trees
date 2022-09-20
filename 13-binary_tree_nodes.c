bee9c2ccb930e7b771fd4711c31904f20b67dd0d
Simret <simruineb@gmail.com>
2022-09-20 02:36:15 -0700

    Edit

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
 	if (tree->left != NULL || tree->right != NULL)
 	{
 		n = 1 + binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left);
