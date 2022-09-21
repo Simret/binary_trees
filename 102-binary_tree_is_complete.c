444fb110dfae2591cf3bff7c6ead659c763b20a9
Simret <simruineb@gmail.com>
2022-09-21 15:39:29 -0700

    Betty

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -13,7 +13,6 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
 	size = binary_tree_size(tree);
 	return (check(tree, i, size));
 }
@@ -32,10 +31,8 @@ int check(const binary_tree_t *tree, unsigned int index, size_t size)
 
 	if (tree == NULL)
 		return (1);
-
 	if (index >= size)
 		return (0);
-
 	isComplete = check(tree->left, 2 * index + 1, size) && check(tree->right, 2 * index + 2, size);
 	return (isComplete);
 }
@@ -52,8 +49,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
 	size = 1 + binary_tree_size(tree->right) + binary_tree_size(tree->left);
-
 	return (size);
 }
