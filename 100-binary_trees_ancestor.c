6083905c2c9ef7c6d79fd9040bc4fa3cb886e9c0
Simret <simruineb@gmail.com>
2022-09-21 15:04:55 -0700

    Betty

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -14,8 +14,10 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 
 	if (first == NULL || second == NULL)
 		return (NULL);
+
 	d_a = binary_tree_depth(a);
 	d_b = binary_tree_depth(b);
+
 	while (d_b > d_a)
 		b = b->parent, d_b--;
 	while (d_a > d_b)
@@ -42,6 +44,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+
 	while (tree->parent != NULL)
 	{
 		depth++;
