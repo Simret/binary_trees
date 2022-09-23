a4d19eee6acadbdd2fe61f06d329aa3ddf3921ee
Simret <simruineb@gmail.com>
2022-09-23 05:08:12 -0700

    Insert value

--- binary_trees.h
+++ binary_trees.h
@@ -56,11 +56,12 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
 
 size_t measure(const binary_tree_t *tree);
 int max(int a, int b);
 int check(const binary_tree_t *tree, unsigned int index, size_t size);
-int btia_helper(const binary_tree_t *tree, int min, int max);
+int btia_helper(const binary_tree_t *tree, int min int max);
 
 #endif /* BINARY_T_H */
 
