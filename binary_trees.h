2eda63def6d5bad509fc63e1715bf1377af8f406
Simret <simruineb@gmail.com>
2022-09-23 06:57:05 -0700

    Build from array

--- binary_trees.h
+++ binary_trees.h
@@ -58,6 +58,7 @@ bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval);
 
 size_t measure(const binary_tree_t *tree);
