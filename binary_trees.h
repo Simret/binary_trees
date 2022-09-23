f16558a9181ece770224bc59445a3c5863e63a7d
Simret <simruineb@gmail.com>
2022-09-23 04:28:55 -0700

    Check binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -55,9 +55,12 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 size_t measure(const binary_tree_t *tree);
 int max(int a, int b);
 int check(const binary_tree_t *tree, unsigned int index, size_t size);
+int btia_helper(const binary_tree_t *tree, int min, int max);
 
 #endif /* BINARY_T_H */
+
