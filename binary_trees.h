f5282cd73e9ccca86eb0b757d474ebb15625ae99
Simret <simruineb@gmail.com>
2022-09-22 18:27:03 -0700

    Build a binary search tree from array

--- binary_trees.h
+++ binary_trees.h
@@ -52,8 +52,8 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
-bst_t *bst_search(const bst_t *tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
 
 size_t measure(const binary_tree_t *tree);
 int max(int a, int b);
